311ab6cdb9627ea7b498ecdcf459b45ae6fd28ce
Tomáš Formánek <tomas.formanek@obspm.fr>
2025-10-28 09:51:10 +0100

    transport eq

---
+++ Meliani/transport_equation.py
@@ -0,0 +1,125 @@
+"""
+Implementation of a Euler scheme solver of the 1D advection equation.
+Equation: 
+∂_t u + a_0 ∂_x u = 0
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+### Parameters ###
+a0 = 2e5 # m/s
+L = 1.5e11 # m (1 au)
+sigma = 0.1*L
+nsigma = 0.1 # normalised
+numax = 1 # normalised max speed
+ttravel = L/a0
+ttravel_days = ttravel/86400
+
+class InitialProfile:
+    def __init__(self, profile_type='gaussian', boundary_conditions='periodic'):
+        self.profile_type = profile_type
+        self.boundary_conditions = boundary_conditions
+
+    def set_profile_parameters(self, **kwargs):
+        if self.profile_type == 'gaussian':
+            self.set_gaussian_profile(kwargs.get('grid', np.linspace(0,1,1000)),
+                                      kwargs.get('x0', 0.1),
+                                      kwargs.get('sigma', 0.1))
+            
+    def set_gaussian_profile(self, grid, x0, sigma):
+        self.x0 = x0
+        self.sigma = sigma
+        self.grid = grid
+        self.profile = self.Gaussian(grid, x0, sigma)
+        if self.boundary_conditions == 'periodic':
+            self.profile += self.Gaussian(grid + 1, x0, sigma) + self.Gaussian(grid - 1, x0, sigma) # simple periodic extension by one period
+
+
+    def Gaussian(self, x, mu, sigma):
+        return np.exp(-0.5*((x - mu)/sigma)**2) # max = 1 - normalised
+    
+    def get_physical_profile(self):
+        raise NotImplementedError('Physical profile conversion not implemented yet.')
+    
+    def set_profile(self, new_profile):
+        self.profile = new_profile
+        
+    def plot_profile(self):
+        plt.plot(self.grid, self.profile)
+        plt.xlabel('Normalised Distance')
+        plt.ylabel('Normalised Speed')
+        plt.title(f'Initial Profile: {self.profile_type}')
+        plt.grid()
+        plt.show()
+
+    
+
+class TransportEquation1D:
+    ############################################################################
+    def __init__(self, a0, L, Nx, Ccond, initial_profile):
+        self.a0 = a0
+        self.L = L
+        self.na0 = a0 / (L) # normalised a0
+        self.Nx = Nx
+        self.Ccond = Ccond
+        self.profile = initial_profile
+
+        self.grid = initial_profile.grid
+
+    def determine_time_step(self):
+        if self.Ccond > 1:
+            print("WARNING: CFL condition violated: Ccond should be chosen at least ≤ 1 for stability.")
+        dxphys = self.L / self.Nx
+        dt = self.Ccond * dxphys / self.a0 # a0 also in physical units
+        return dt
+    
+    def run_simulation(self, tmax):
+        dt = self.determine_time_step()
+        nsteps = int(tmax / dt)
+        u = self.profile.profile
+        dx = 1 / self.Nx
+
+        for n in range(nsteps):
+            # update the points from 1 to end-1 (the first and last points are for boundary conditions)
+            du = self.du(u, dx, dt)
+            u[1:-1] += du
+            if self.profile.boundary_conditions == 'periodic':
+                u[0] = u[-2]  # periodic BC
+                u[-1] = u[1]
+
+            if n%10 == 0:
+                print(f'Step {n}/{nsteps}, Time {n*dt:.2f}s')
+                print(self.profile.profile[0:10])
+                self.profile.set_profile(u)
+                self.profile.plot_profile()
+
+
+
+    def du_old(self, u, dx, dt):
+        """
+        Implementation using central difference (not stable for advection equation).
+        """
+        dudx = np.zeros(np.shape(u)[0] - 2)
+        dudx = (u[2:] - u[:-2]) / (2*dx)
+        return -dt * self.na0 * dudx
+
+    def du(self, u, dx, dt):
+        """
+        Implementation using upwind scheme. This is stable, but why? The information used to update u[i] only comes from u[i] and u[i-1], meaning along the direction of propagation. 
+        This avoids introducing spurious oscillations that can occur when using central differences, which use information from both sides of the point being updated.
+        """
+        C = self.na0 * dt / dx
+        dudx = u[1:-1] - u[:-2]  # upwind
+        return -C * dudx
+
+
+if __name__ == "__main__":
+    # Example usage
+    nx = 1000
+    Ccond = 0.8
+    boundary_conditions = 'periodic'
+    initial_profile = InitialProfile(profile_type='gaussian', boundary_conditions=boundary_conditions)
+    initial_profile.set_profile_parameters(grid=np.linspace(0,1,nx), x0=0.1, sigma=nsigma)
+    initial_profile.plot_profile()
+    simulator = TransportEquation1D(a0, L, nx, Ccond, initial_profile)
+    simulator.run_simulation(tmax=ttravel)
